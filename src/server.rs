315e85c83632a797f7b06723fc24ecefbbb040ca
Haileyesus <haileyesusbe@gmail.com>
2025-04-19 15:53:53 -0400

    feat: install necessary crates and write a sample proto file with the server setup

---
+++ src/server.rs
@@ -0,0 +1,37 @@
+use tonic::{Request, Response, Status, transport::Server};
+
+pub mod grpc_server {
+    tonic::include_proto!("hello");
+}
+
+use grpc_server::greeter_server::{Greeter, GreeterServer};
+use grpc_server::{HelloReply, HelloRequest};
+
+#[derive(Debug, Default)]
+pub struct MyGreeter {}
+
+#[tonic::async_trait]
+impl Greeter for MyGreeter {
+    async fn say_hello(
+        &self,
+        request: Request<HelloRequest>,
+    ) -> Result<Response<HelloReply>, Status> {
+        println!("Got Request : {:?}", request);
+        let reply = grpc_server::HelloReply {
+            message: format!("Hello {}!", request.into_inner().name),
+        };
+        Ok(Response::new(reply))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let addr = "[::1]:50051".parse()?;
+    let greeter = MyGreeter::default();
+
+    Server::builder()
+        .add_service(GreeterServer::new(greeter))
+        .serve(addr)
+        .await?;
+    Ok(())
+}
