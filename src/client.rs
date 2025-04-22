5dbcc08ef04c3aa82514183f06d9f4ecfc80ddc3
Haileyesus <haileyesusbe@gmail.com>
2025-04-22 04:48:06 -0400

    feat: setup the client

--- src/client.rs
+++ src/client.rs
@@ -1,1 +1,20 @@
-fn main() {}
+pub mod grpc_server {
+    tonic::include_proto!("hello");
+}
+
+use grpc_server::HelloRequest;
+use grpc_server::greeter_client::GreeterClient;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut client = GreeterClient::connect("http://[::1]:50051").await?;
+
+    let request = tonic::Request::new(HelloRequest {
+        name: "Haile".into(),
+    });
+    let response = client.say_hello(request).await?;
+
+    println!("RESPONSE={:?}", response);
+
+    Ok(())
+}
